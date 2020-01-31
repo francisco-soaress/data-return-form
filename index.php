eee701fbec5feac359d0b7955664dcb241bd9e46
tiesco-assis <francisco.sts@hotmail.com>
2020-01-30 22:25:41 -0300

    Atualizando arquivos

--- index.php
+++ index.php
@@ -1,12 +1,30 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
+    <link rel="stylesheet" href="css/style.css" media="all">
+
     <title>Data Form Return</title>
 </head>
+
 <body>
-    
+
+    <form class="main_form send_form" id="general_form" name="general_form" action="" method="post">
+
+        <label for="">
+            <input type="text" name="" class="" id="" placeholder="" require>
+        </label>
+        <label for="">
+            <input type="text" name="" class="" id="" placeholder="" require>
+        </label>
+        <label for="">
+            <input type="file" name="" class="" value="File" id="" placeholder="File" require>
+        </label>
+    </form>
+
 </body>
+
 </html>
